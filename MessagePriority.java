497329a1910db47b4b42d48a858d4b5815a6d26a
Paul Jonas <Paul.Jonas@dohle-net.de>
2025-09-24 22:00:08 +0200

    docs: add comments to MessagePriority Enum class

--- MessagePriority.java
+++ MessagePriority.java
@@ -1,7 +1,23 @@
+/**
+ * An enumeration representing the priority levels of messages.
+ * They are used to control the display of messages based on their importance.
+ * 
+ * @link MessagePriority#INFO
+ * @link MessagePriority#WARNING
+ * @link MessagePriority#ERROR
+ * @link MessagePriority#NONE
+ * 
+ * @version 24 September 2025
+ * @author Paul Jonas Dohle
+ */
 public enum MessagePriority {
+    /** Informational messages, lowest priority */
     INFO(0),
+    /** Warning messages, medium priority */
     WARNING(1),
+    /** Error messages, highest priority */
     ERROR(2),
+    /** No messages, disables message display */
     NONE(3);
 
     private final int PRIORITY;
@@ -10,6 +26,11 @@ public enum MessagePriority {
         this.PRIORITY = PRIORITY;
     }
 
+    /**
+     * Returns the priority of the message as an integer.
+     * 
+     * @return the priority of the message
+     */
     public int getPriority() {
         return PRIORITY;
     }
