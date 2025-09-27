ea862db4119ad99ba427323f368b6d17748693da
Paul Jonas <Paul.Jonas@dohle-net.de>
2025-09-27 23:15:49 +0200

    feat: add Portal class with act method and associated context file

---
+++ Portal.java
@@ -0,0 +1,16 @@
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A portal can be placed in the world to teleport actors to another portal.
+ * 
+ * @author Paul Jonas
+ * @version 27 September 2025
+ */
+public class Portal extends Actor {
+    /**
+     * If the button "act" in the main window is clicked this method is executed.
+     * The button "run" would execute this method as well, but in a loop.
+     */
+    public void act() {
+    }
+}
